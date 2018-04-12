5224758cdec201bb45c72c8349f82b19d0c4614f
Leon-Home <772153508@qq.com>
2018-04-12 20:38:28 +0800

    完善PersonAppService

--- src/Mpa.Phonebook.Application/PhoneBook/PersonAppService.cs
+++ src/Mpa.Phonebook.Application/PhoneBook/PersonAppService.cs
@@ -1,7 +1,29 @@
-﻿namespace Mpa.Phonebook.PhoneBook
+﻿using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
+using Mpa.Phonebook.PhoneBook.Dtos;
+
+namespace Mpa.Phonebook.PhoneBook
 {
     public class PersonAppService: PhonebookAppServiceBase,IPersonAppService
     {
-        
+        public Task<PagedResultDto<PersonListDto>> GetPagedPersonAsync(GetPersonInput input)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<PersonListDto> GetPersonByIdAsync(GetPersonInput input)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task CreateOrUpdatePersonAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeletePersonByIdAsync(EntityDto input)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
